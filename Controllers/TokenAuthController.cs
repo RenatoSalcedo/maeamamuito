dbe75040306c05bb28f961ff7afc44f9333e3b99
Renato Salcedo <renatoms@infoserver.com>
2017-05-26 16:20:28 -0300

    continuação implement authenticate.

--- Controllers/TokenAuthController.cs
+++ Controllers/TokenAuthController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading.Tasks;
-using maeamamuito.Core.Model;
+using CSTokenBaseAuth.Auth;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 
 namespace WebApplicationBasic.Controllers
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Data.SqlClient;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Newtonsoft.Json;
+using CSTokenBaseAuth.Auth;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebApplicationBasic
 {
